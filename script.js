3096aff0205dd68dafc60cf53d3d0cc6fbe19585
Zofiant <150848338+Zofiant@users.noreply.github.com>
2023-12-10 23:27:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+document.querySelector('.burger').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.nav_links').classList.toggle('open');
+})
+
+
+
+
+
+
+
+
+// function footer() {
+//     const
+//         main = document.getElementsByTagName('main')[0],
+//         footer = document.getElementsByTagName('footer')[0]
+
+//     main.style.paddingBottom = footer.clientHeight + 'px'
+// }
+
+// window.addEventListener('load', footer);
+// window.addEventListener('resize', footer);
+
+
+// импортируем слова из файла
